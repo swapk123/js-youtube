5bc8713b0aba799827dd863dfa1465d74f5bfb20
Swapna Kochrekar <88872090+swapk123@users.noreply.github.com>
2023-11-15 08:05:08 +0000

    #variable new file added

---
+++ 01_jsbasic/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 1234;
+
+let accountEmail = "anagha@gmail.com";
+
+var accountCity = "udupi";
+
+// accountState = "karnataka";
+
+// accountEmail = "sonali@gmail.com";
+
+//accountCity = "manglore";
+
+/* preper not to use var because of 
+issue in block scope and functional scope.*/
+
+console.table([accountId, accountEmail, accountCity, accountState])
