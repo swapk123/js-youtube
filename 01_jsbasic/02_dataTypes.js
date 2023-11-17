439b14c5f707c2929c4605d69ca98017cd352018
Swapna Kochrekar <88872090+swapk123@users.noreply.github.com>
2023-11-17 10:31:40 +0000

    added dataTypes new files

---
+++ 01_jsbasic/02_dataTypes.js
@@ -0,0 +1,13 @@
+const accountName="sonali kadolkar";
+
+const accountEmail="sonali@gmail.com";
+
+const accountCity="jaipur";
+
+const state="rajastan";
+
+const userLoggedIn=false;
+
+const accountId=123456;
+
+console.table([ accountId,  accountName,  accountEmail,  accountCity,  state, userLoggedIn])
